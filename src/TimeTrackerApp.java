bd54d127a0d4728f0723e4ce5fe3d3ba54756bb7
robinsiebert <uyqmv@student.kit.edu>
2024-12-11 20:33:36 +0100

    Current status

--- src/TimeTrackerApp.java
+++ src/TimeTrackerApp.java
@@ -2,12 +2,13 @@ public class TimeTrackerApp {
     public static void main(String[] args) {
         DatabaseHandler dbHandler = new DatabaseHandler();
 
+        /*
+
         // Beispiel: Neues Thema hinzufügen
         dbHandler.addTheme("Project Work");
 
-
         // Beispiel: Neuen Zeiteintrag hinzufügen
-        dbHandler.addTimeEntry("2024-12-11", "09:00", "17:00", "Development", 8.0, "Project Work");
+        dbHandler.addTimeEntry("2024-12-12", "09:00", "17:00", "Development", 8.0, "Project Work");
 
         // Beispiel: Alle Einträge anzeigen
         dbHandler.showAllEntries();
@@ -17,6 +18,11 @@ public class TimeTrackerApp {
 
         // Beispiel: Alle Einträge anzeigen
         dbHandler.showAllEntries();
+
+        // Beispiel: Ein Zeiteintrag bearbeiten
+        dbHandler.updateTimeEntry(1, "2024-12-12", "08:00", "16:00", "Updated Project", 8.0, "Updated Theme");
+
+         */
 
         // Verbindung schließen
         dbHandler.close();
